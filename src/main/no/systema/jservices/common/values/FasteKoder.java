f923e1f185ef9cb301db1e768ba3d713f107c5aa
Fredrik Möller <fredrik@systema.no>
2016-11-24 13:28:52 +0100

    Init commit

---
+++ src/main/no/systema/jservices/common/values/FasteKoder.java
@@ -0,0 +1,21 @@
+package no.systema.jservices.common.values;
+
+import java.io.Serializable;
+
+/**
+ * Holding constant for select section from ...
+ * 
+ * 
+ * AIRCOD AIRSPH ARKIVJ ARKIVU AVLETEKST BERKOD CPSORT DELSYS ESPEDPARM
+ * FRBREVPRO FRBREVPROT FRTKOD FSDOKK FUNKSJON GEBTYP HEUR INLTOPOPT KOLTYP
+ * LAGERLEIER LAGERMOT LAGERUTTAK MLAPKOD MST PP01DAT03 PRKOLL SEASYS SFEDIT
+ * STATKODE STSORT ST01TYPE SVTULL1 SYPAR S8SATSTY TABTYP TRACKPREFI TRACKSUFFI
+ * TRACKT UTLKOD WEBETRACK XMLVLTYPE
+ * 
+ * @author Fredrik Möller
+ * @date Nov 21, 2016
+ *
+ */
+public enum FasteKoder implements Serializable {
+	FUNKSJON
+}
