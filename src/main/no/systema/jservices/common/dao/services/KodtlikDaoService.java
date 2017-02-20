9dd3a570eb14cd16fb52c518704cd6132553d33e
Fredrik Möller <fredrik@systema.no>
2017-02-20 10:03:45 +0100

    Added KodtlikDao

---
+++ src/main/no/systema/jservices/common/dao/services/KodtlikDaoService.java
@@ -0,0 +1,7 @@
+package no.systema.jservices.common.dao.services;
+
+import no.systema.jservices.common.dao.KodtlikDao;
+
+public interface KodtlikDaoService extends GenericDaoService<KodtlikDao> {
+	
+}
---
+++ src/main/no/systema/jservices/common/dao/services/KodtlikDaoServiceImpl.java
@@ -0,0 +1,7 @@
+package no.systema.jservices.common.dao.services;
+
+import no.systema.jservices.common.dao.KodtlikDao;
+
+public class KodtlikDaoServiceImpl extends GenericDaoServiceImpl<KodtlikDao> implements KodtlikDaoService{
+
+}
