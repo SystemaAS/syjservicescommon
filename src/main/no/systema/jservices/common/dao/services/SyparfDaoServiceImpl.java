3f93f04864621f49a244ad38427c5cb0a761f68f
Fredrik Möller <fredrik@systema.no>
2017-03-03 14:00:46 +0100

    Enhancement of Syparf

--- src/main/no/systema/jservices/common/dao/services/SyparfDaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/SyparfDaoServiceImpl.java
@@ -1,7 +1,26 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import no.systema.jservices.common.dao.SyparfDao;
 
 public class SyparfDaoServiceImpl extends GenericDaoServiceImpl<SyparfDao> implements SyparfDaoService{
+
+	@Override
+	public List<SyparfDao> findAll(String sykunr) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("sykunr", sykunr);
+		return findAll(params);
+	}
+
+	@Override
+	public SyparfDao find(String sykunr, String syrecn) {
+		SyparfDao qDao = new SyparfDao();
+		qDao.setSykunr(sykunr);
+		qDao.setSyrecn(syrecn);
+		return find(qDao);
+	}
 
 }
