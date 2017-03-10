c861fb8048ebd7e606ec99ab017d8ad3fefa37f4
Oscar de la Torre <oscar@systema.no>
2017-03-10 14:17:50 +0100

    Update

--- src/main/no/systema/jservices/common/dao/DktardDao.java
+++ src/main/no/systema/jservices/common/dao/DktardDao.java
@@ -17,195 +17,195 @@ public class DktardDao implements IDao {
 
 	private String dktard01 = "";                                
 	public void setDktard01 (String value){ this.dktard01 = value;   }   
-	public String getDktard01 (){ return this.dktard01;   }  
+	public String getDktard01 (){ return this.dktard01.trim();   }  
 	
 	private String dktard02 = "";                                
 	public void setDktard02 (String value){ this.dktard02 = value;   }   
-	public String getDktard02 (){ return this.dktard02;   }  
+	public String getDktard02 (){ return this.dktard02.trim();   }  
 	
 	private String dktard03 = "";                                
 	public void setDktard03 (String value){ this.dktard03 = value;   }   
-	public String getDktard03 (){ return this.dktard03;   }  
+	public String getDktard03 (){ return this.dktard03.trim();   }  
 	
 	private String dktard04 = "";                                
 	public void setDktard04 (String value){ this.dktard04 = value;   }   
-	public String getDktard04 (){ return this.dktard04;   }  
+	public String getDktard04 (){ return this.dktard04.trim();   }  
 	
 	private String dktard05 = "";                                
 	public void setDktard05 (String value){ this.dktard05 = value;   }   
-	public String getDktard05 (){ return this.dktard05;   }  
+	public String getDktard05 (){ return this.dktard05.trim();   }  
 	
 	private String dktard06 = "";                                
 	public void setDktard06 (String value){ this.dktard06 = value;   }   
-	public String getDktard06 (){ return this.dktard06;   }  
+	public String getDktard06 (){ return this.dktard06.trim();   }  
 	
 	private String dktard07 = "";                                
 	public void setDktard07 (String value){ this.dktard07 = value;   }   
-	public String getDktard07 (){ return this.dktard07;   }  
+	public String getDktard07 (){ return this.dktard07.trim();   }  
 	
 	private String dktard08 = "";                                
 	public void setDktard08 (String value){ this.dktard08 = value;   }   
-	public String getDktard08 (){ return this.dktard08;   }  
+	public String getDktard08 (){ return this.dktard08.trim();   }  
 	
 	private String dktard09 = "";                                
 	public void setDktard09 (String value){ this.dktard09 = value;   }   
-	public String getDktard09 (){ return this.dktard09;   }  
+	public String getDktard09 (){ return this.dktard09.trim();   }  
 	
 	private String dktard10 = "";                                
 	public void setDktard10 (String value){ this.dktard10 = value;   }   
-	public String getDktard10 (){ return this.dktard10;   }  
+	public String getDktard10 (){ return this.dktard10.trim();   }  
 	
 	private String dktard11 = "";                                
 	public void setDktard11 (String value){ this.dktard11 = value;   }   
-	public String getDktard11 (){ return this.dktard11;   }  
+	public String getDktard11 (){ return this.dktard11.trim();   }  
 	
 	private String dktard12 = "";                                
 	public void setDktard12 (String value){ this.dktard12 = value;   }   
-	public String getDktard12 (){ return this.dktard12;   }  
+	public String getDktard12 (){ return this.dktard12.trim();   }  
 	
 	private String dktard13 = "";                                
 	public void setDktard13 (String value){ this.dktard13 = value;   }   
-	public String getDktard13 (){ return this.dktard13;   }  
+	public String getDktard13 (){ return this.dktard13.trim();   }  
 	
 	private String dktard14 = "";                                
 	public void setDktard14 (String value){ this.dktard14 = value;   }   
-	public String getDktard14 (){ return this.dktard14;   }  
+	public String getDktard14 (){ return this.dktard14.trim();   }  
 	
 	private String dktard15 = "";                                
 	public void setDktard15 (String value){ this.dktard15 = value;   }   
-	public String getDktard15 (){ return this.dktard15;   }  
+	public String getDktard15 (){ return this.dktard15.trim();   }  
 	
 	private String dktard16 = "";                                
 	public void setDktard16 (String value){ this.dktard16 = value;   }   
-	public String getDktard16 (){ return this.dktard16;   }  
+	public String getDktard16 (){ return this.dktard16.trim();   }  
 	
 	private String dktard17 = "";                                
 	public void setDktard17 (String value){ this.dktard17 = value;   }   
-	public String getDktard17 (){ return this.dktard17;   }  
+	public String getDktard17 (){ return this.dktard17.trim();   }  
 	
 	private String dktard18 = "";                                
 	public void setDktard18 (String value){ this.dktard18 = value;   }   
-	public String getDktard18 (){ return this.dktard18;   }  
+	public String getDktard18 (){ return this.dktard18.trim();   }  
 	
 	private String dktard19 = "";                                
 	public void setDktard19 (String value){ this.dktard19 = value;   }   
-	public String getDktard19 (){ return this.dktard19;   }  
+	public String getDktard19 (){ return this.dktard19.trim();   }  
 	
 	private String dktard20 = "";                                
 	public void setDktard20 (String value){ this.dktard20 = value;   }   
-	public String getDktard20 (){ return this.dktard20;   }  
+	public String getDktard20 (){ return this.dktard20.trim();   }  
 	
 	private String dktard21 = "";                                
 	public void setDktard21 (String value){ this.dktard21 = value;   }   
-	public String getDktard21 (){ return this.dktard21;   }  
+	public String getDktard21 (){ return this.dktard21.trim();   }  
 	
 	private String dktard22 = "";                                
 	public void setDktard22 (String value){ this.dktard22 = value;   }   
-	public String getDktard22 (){ return this.dktard22;   }  
+	public String getDktard22 (){ return this.dktard22.trim();   }  
 	
 	private String dktard23 = "";                                
 	public void setDktard23 (String value){ this.dktard23 = value;   }   
-	public String getDktard23 (){ return this.dktard23;   }  
+	public String getDktard23 (){ return this.dktard23.trim();   }  
 	
 	private String dktard24 = "";                                
 	public void setDktard24 (String value){ this.dktard24 = value;   }   
-	public String getDktard24 (){ return this.dktard24;   }  
+	public String getDktard24 (){ return this.dktard24.trim();   }  
 	
 	private String dktard25 = "";                                
 	public void setDktard25 (String value){ this.dktard25 = value;   }   
-	public String getDktard25 (){ return this.dktard25;   }  
+	public String getDktard25 (){ return this.dktard25.trim();   }  
 	
 	private String dktard26 = "";                                
 	public void setDktard26 (String value){ this.dktard26 = value;   }   
-	public String getDktard26 (){ return this.dktard26;   }  
+	public String getDktard26 (){ return this.dktard26.trim();  }  
 	
 	private String dktard27 = "";                                
 	public void setDktard27 (String value){ this.dktard27 = value;   }   
-	public String getDktard27 (){ return this.dktard27;   }  
+	public String getDktard27 (){ return this.dktard27.trim();   }  
 	
 	private String dktard28 = "";                                
 	public void setDktard28 (String value){ this.dktard28 = value;   }   
-	public String getDktard28 (){ return this.dktard28;   }  
+	public String getDktard28 (){ return this.dktard28.trim();   }  
 	
 	private String dktard29 = "";                                
 	public void setDktard29 (String value){ this.dktard29 = value;   }   
-	public String getDktard29 (){ return this.dktard29;   }  
+	public String getDktard29 (){ return this.dktard29.trim();   }  
 	
 	private String dktard30 = "";                                
 	public void setDktard30 (String value){ this.dktard30 = value;   }   
-	public String getDktard30 (){ return this.dktard30;   }  
+	public String getDktard30 (){ return this.dktard30.trim();  }  
 	
 	private String dktard31 = "";                                
 	public void setDktard31 (String value){ this.dktard31 = value;   }   
-	public String getDktard31 (){ return this.dktard31;   }  
+	public String getDktard31 (){ return this.dktard31.trim();   }  
 	
 	private String dktard32 = "";                                
 	public void setDktard32 (String value){ this.dktard32 = value;   }   
-	public String getDktard32 (){ return this.dktard32;   }  
+	public String getDktard32 (){ return this.dktard32.trim();   }  
 	
 	private String dktard33 = "";                                
 	public void setDktard33 (String value){ this.dktard33 = value;   }   
-	public String getDktard33 (){ return this.dktard33;   }  
+	public String getDktard33 (){ return this.dktard33.trim();   }  
 	
 	private String dktard34 = "";                                
 	public void setDktard34 (String value){ this.dktard34 = value;   }   
-	public String getDktard34 (){ return this.dktard34;   }  
+	public String getDktard34 (){ return this.dktard34.trim();   }  
 	
 	private String dktard35 = "";                                
 	public void setDktard35 (String value){ this.dktard35 = value;   }   
-	public String getDktard35 (){ return this.dktard35;   }  
+	public String getDktard35 (){ return this.dktard35.trim();   }  
 	
 	private String dktard36 = "";                                
 	public void setDktard36 (String value){ this.dktard36 = value;   }   
-	public String getDktard36 (){ return this.dktard36;   }  
+	public String getDktard36 (){ return this.dktard36.trim();   }  
 	
 	private String dktard37 = "";                                
 	public void setDktard37 (String value){ this.dktard37 = value;   }   
-	public String getDktard37 (){ return this.dktard37;   }  
+	public String getDktard37 (){ return this.dktard37.trim();   }  
 	
 	private String dktard38 = "";                                
 	public void setDktard38 (String value){ this.dktard38 = value;   }   
-	public String getDktard38 (){ return this.dktard38;   }  
+	public String getDktard38 (){ return this.dktard38.trim();   }  
 	
 	private String dktard39 = "";                                
 	public void setDktard39 (String value){ this.dktard39 = value;   }   
-	public String getDktard39 (){ return this.dktard39;   }  
+	public String getDktard39 (){ return this.dktard39.trim();   }  
 	
 	private String dktard40 = "";                                
 	public void setDktard40 (String value){ this.dktard40 = value;   }   
-	public String getDktard40 (){ return this.dktard40;   }  
+	public String getDktard40 (){ return this.dktard40.trim();   }  
 	
 	private String dktard41 = "";                                
 	public void setDktard41 (String value){ this.dktard41 = value;   }   
-	public String getDktard41 (){ return this.dktard41;   }  
+	public String getDktard41 (){ return this.dktard41.trim();   }  
 	
 	private String dktard42 = "";                                
 	public void setDktard42 (String value){ this.dktard42 = value;   }   
-	public String getDktard42 (){ return this.dktard42;   }  
+	public String getDktard42 (){ return this.dktard42.trim();   }  
 	
 	private String dktard43 = "";                                
 	public void setDktard43 (String value){ this.dktard43 = value;   }   
-	public String getDktard43 (){ return this.dktard43;   }  
+	public String getDktard43 (){ return this.dktard43.trim();   }  
 	
 	private String dktard44 = "";                                
 	public void setDktard44 (String value){ this.dktard44 = value;   }   
-	public String getDktard44 (){ return this.dktard44;   }  
+	public String getDktard44 (){ return this.dktard44.trim();   }  
 	
 	private String dktard45 = "";                                
 	public void setDktard45 (String value){ this.dktard45 = value;   }   
-	public String getDktard45 (){ return this.dktard45;   }  
+	public String getDktard45 (){ return this.dktard45.trim();   }  
 	
 	private String dktard46 = "";                                
 	public void setDktard46 (String value){ this.dktard46 = value;   }   
-	public String getDktard46 (){ return this.dktard46;   }  
+	public String getDktard46 (){ return this.dktard46.trim();   }  
 	
 	private String dktard47 = "";                                
 	public void setDktard47 (String value){ this.dktard47 = value;   }   
-	public String getDktard47 (){ return this.dktard47;   }  
+	public String getDktard47 (){ return this.dktard47.trim();   }  
 	
 	private String dktard48 = "";                                
 	public void setDktard48 (String value){ this.dktard48 = value;   }   
-	public String getDktard48 (){ return this.dktard48;   }  
+	public String getDktard48 (){ return this.dktard48.trim();   }  
 	
 	
 	Map<String, Object> keys = new HashMap<String, Object>();
