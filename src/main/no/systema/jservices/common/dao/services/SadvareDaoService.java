72fe3e05a9fa720367bedd5b874243323bad819c
Fredrik Möller <fredrik@systema.no>
2017-03-21 14:02:58 +0100

    Added find in SadvareDaoImpl

--- src/main/no/systema/jservices/common/dao/services/SadvareDaoService.java
+++ src/main/no/systema/jservices/common/dao/services/SadvareDaoService.java
@@ -1,7 +1,26 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.List;
+
 import no.systema.jservices.common.dao.SadvareDao;
 
 public interface SadvareDaoService extends GenericDaoService<SadvareDao> {
+	
+	/**
+	 * Get unique sadvare
+	 * 
+	 * @param levenr, customer
+	 * @param varenr, article number
+	 * @return a {@link SadvareDao}, i found, else null
+	 */
+	public SadvareDao find(String levenr, String varenr);
+	
+	/**
+	 * Retrieve all sadvare for levenr, customer
+	 * 
+	 * @param levenr, actual customer
+	 * @return a List of {@link SadvareDao}, could be empty
+	 */
+	public List<SadvareDao> findAll(String sykunr);	
 	
 }
--- src/main/no/systema/jservices/common/dao/services/SadvareDaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/SadvareDaoServiceImpl.java
@@ -1,7 +1,26 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import no.systema.jservices.common.dao.SadvareDao;
 
 public class SadvareDaoServiceImpl extends GenericDaoServiceImpl<SadvareDao> implements SadvareDaoService{
+
+	@Override
+	public SadvareDao find(String levenr, String varenr) {
+		SadvareDao qDao = new SadvareDao();
+		qDao.setLevenr(levenr);
+		qDao.setVarenr(varenr);
+		return find(qDao);
+	}
+
+	@Override
+	public List<SadvareDao> findAll(String levenr) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("levenr", levenr);
+		return findAll(params);
+	}
 
 }
