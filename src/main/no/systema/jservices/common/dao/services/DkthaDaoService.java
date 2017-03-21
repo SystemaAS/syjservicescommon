a894fa32a411de07a5a35c44b8e7bb52fff2a7cb
Oscar de la Torre <oscar@systema.no>
2017-03-21 10:00:59 +0100

    Update

--- src/main/no/systema/jservices/common/dao/services/DkthaDaoService.java
+++ src/main/no/systema/jservices/common/dao/services/DkthaDaoService.java
@@ -2,8 +2,8 @@ package no.systema.jservices.common.dao.services;
 
 import java.util.List;
 
-import no.systema.jservices.common.dao.DktardDao;
+import no.systema.jservices.common.dao.DkthaDao;
 
-public interface DkthaDaoService extends GenericDaoService<DktardDao> {
+public interface DkthaDaoService extends GenericDaoService<DkthaDao> {
 	
 }
--- src/main/no/systema/jservices/common/dao/services/DkthaDaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/DkthaDaoServiceImpl.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import no.systema.jservices.common.dao.DktardDao;
+import no.systema.jservices.common.dao.DkthaDao;
 
-public class DkthaDaoServiceImpl extends GenericDaoServiceImpl<DktardDao> implements DkthaDaoService{
+public class DkthaDaoServiceImpl extends GenericDaoServiceImpl<DkthaDao> implements DkthaDaoService{
 
 
 }
