94d855a567ce611fe0242933717d25217e98fc06
Fredrik Möller <fredrik@systema.no>
2017-04-06 11:46:38 +0200

    Enhancement in KODTS8

--- src/main/no/systema/jservices/common/dao/services/Kodts8DaoService.java
+++ src/main/no/systema/jservices/common/dao/services/Kodts8DaoService.java
@@ -1,7 +1,16 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.List;
+
 import no.systema.jservices.common.dao.Kodts8Dao;
 
 public interface Kodts8DaoService extends GenericDaoService<Kodts8Dao> {
 	
+	/**
+	 * Retrieves all Kodts8Dao where ks8skv is not null
+	 * 
+	 * @return a list with Kodts8Dao
+	 */
+	public List<Kodts8Dao> getKoder();
+
 }
--- src/main/no/systema/jservices/common/dao/services/Kodts8DaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/Kodts8DaoServiceImpl.java
@@ -1,7 +1,16 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.List;
+
+import no.systema.jservices.common.dao.GenericObjectMapper;
 import no.systema.jservices.common.dao.Kodts8Dao;
 
 public class Kodts8DaoServiceImpl extends GenericDaoServiceImpl<Kodts8Dao> implements Kodts8DaoService{
+
+	@Override
+	public List<Kodts8Dao> getKoder() {
+		StringBuilder queryString = new StringBuilder("SELECT * FROM kodts8 WHERE NULLIF(ks8skv, '') IS NOT NULL");
+		return findAll(queryString.toString(), new GenericObjectMapper(new Kodts8Dao()));
+	}
 
 }
