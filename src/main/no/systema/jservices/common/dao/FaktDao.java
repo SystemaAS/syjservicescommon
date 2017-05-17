93e880de7fcaadcce6c8ea6a9d854b0fe966c243
Fredrik Möller <fredrik@systema.no>
2017-05-17 09:23:20 +0200

    Added more fields into HEADF o FAKT

--- src/main/no/systema/jservices/common/dao/FaktDao.java
+++ src/main/no/systema/jservices/common/dao/FaktDao.java
@@ -9,9 +9,323 @@ public class FaktDao implements IDao {
 	private int faopd; // key
 	private int fali; // key
 	private String faval;
-	private int fabelv;
 	private int fadato;
 	private String fakda;
+	private String fask;
+	private String favk;
+	private String favt;
+	private String fakdm;
+	private int fabeln;
+	private String fakdul;
+	private String fa01b;
+	private String fakduk;
+	private int fabelu;
+	private int fakunr;
+	private int fafakt;
+	private String faopko;
+	private int fajour;
+	private int facc;
+	private int famnd;
+	private int fafrbn;
+	private int fadaff;
+	private int faavdr;
+	private int falevn;
+	private String famade;
+	private int faagrn;
+	private int faaccn;
+	private int faexra;
+	private int fataxa;
+	private String facuri;
+	private int faamou;
+	private String facd12;
+	private String facd13;
+	private String facd21;
+	private String facd22;
+	private String facd23;
+	private String facd31;
+	private String facd32;
+	private String facu33;
+	private String faggrp;
+
+	public int getFafakt() {
+		return fafakt;
+	}
+
+	public void setFafakt(int fafakt) {
+		this.fafakt = fafakt;
+	}
+
+	public String getFacu33() {
+		return facu33;
+	}
+
+	public void setFacu33(String facu33) {
+		this.facu33 = facu33;
+	}
+
+	public String getFacd12() {
+		return facd12;
+	}
+
+	public void setFacd12(String facd12) {
+		this.facd12 = facd12;
+	}
+
+	public String getFacd13() {
+		return facd13;
+	}
+
+	public void setFacd13(String facd13) {
+		this.facd13 = facd13;
+	}
+
+	public String getFacd21() {
+		return facd21;
+	}
+
+	public void setFacd21(String facd21) {
+		this.facd21 = facd21;
+	}
+
+	public String getFacd22() {
+		return facd22;
+	}
+
+	public void setFacd22(String facd22) {
+		this.facd22 = facd22;
+	}
+
+	public String getFacd23() {
+		return facd23;
+	}
+
+	public void setFacd23(String facd23) {
+		this.facd23 = facd23;
+	}
+
+	public String getFacd31() {
+		return facd31;
+	}
+
+	public void setFacd31(String facd31) {
+		this.facd31 = facd31;
+	}
+
+	public String getFacd32() {
+		return facd32;
+	}
+
+	public void setFacd32(String facd32) {
+		this.facd32 = facd32;
+	}
+
+	public String getFaggrp() {
+		return faggrp;
+	}
+
+	public void setFaggrp(String faggrp) {
+		this.faggrp = faggrp;
+	}
+
+	public int getFafrbn() {
+		return fafrbn;
+	}
+
+	public void setFafrbn(int fafrbn) {
+		this.fafrbn = fafrbn;
+	}
+
+	public int getFadaff() {
+		return fadaff;
+	}
+
+	public void setFadaff(int fadaff) {
+		this.fadaff = fadaff;
+	}
+
+	public int getFaavdr() {
+		return faavdr;
+	}
+
+	public void setFaavdr(int faavdr) {
+		this.faavdr = faavdr;
+	}
+
+	public int getFalevn() {
+		return falevn;
+	}
+
+	public void setFalevn(int falevn) {
+		this.falevn = falevn;
+	}
+
+	public String getFamade() {
+		return famade;
+	}
+
+	public void setFamade(String famade) {
+		this.famade = famade;
+	}
+
+	public int getFaagrn() {
+		return faagrn;
+	}
+
+	public void setFaagrn(int faagrn) {
+		this.faagrn = faagrn;
+	}
+
+	public int getFaaccn() {
+		return faaccn;
+	}
+
+	public void setFaaccn(int faaccn) {
+		this.faaccn = faaccn;
+	}
+
+	public int getFaexra() {
+		return faexra;
+	}
+
+	public void setFaexra(int faexra) {
+		this.faexra = faexra;
+	}
+
+	public int getFataxa() {
+		return fataxa;
+	}
+
+	public void setFataxa(int fataxa) {
+		this.fataxa = fataxa;
+	}
+
+	public String getFacuri() {
+		return facuri;
+	}
+
+	public void setFacuri(String facuri) {
+		this.facuri = facuri;
+	}
+
+	public int getFaamou() {
+		return faamou;
+	}
+
+	public void setFaamou(int faamou) {
+		this.faamou = faamou;
+	}
+
+	public String getFakdul() {
+		return fakdul;
+	}
+
+	public void setFakdul(String fakdul) {
+		this.fakdul = fakdul;
+	}
+
+	public String getFa01b() {
+		return fa01b;
+	}
+
+	public void setFa01b(String fa01b) {
+		this.fa01b = fa01b;
+	}
+
+	public String getFakduk() {
+		return fakduk;
+	}
+
+	public void setFakduk(String fakduk) {
+		this.fakduk = fakduk;
+	}
+
+	public int getFabelu() {
+		return fabelu;
+	}
+
+	public void setFabelu(int fabelu) {
+		this.fabelu = fabelu;
+	}
+
+	public int getFakunr() {
+		return fakunr;
+	}
+
+	public void setFakunr(int fakunr) {
+		this.fakunr = fakunr;
+	}
+
+	public String getFaopko() {
+		return faopko;
+	}
+
+	public void setFaopko(String faopko) {
+		this.faopko = faopko;
+	}
+
+	public int getFajour() {
+		return fajour;
+	}
+
+	public void setFajour(int fajour) {
+		this.fajour = fajour;
+	}
+
+	public int getFacc() {
+		return facc;
+	}
+
+	public void setFacc(int facc) {
+		this.facc = facc;
+	}
+
+	public int getFamnd() {
+		return famnd;
+	}
+
+	public void setFamnd(int famnd) {
+		this.famnd = famnd;
+	}
+
+	public String getFakdm() {
+		return fakdm;
+	}
+
+	public void setFakdm(String fakdm) {
+		this.fakdm = fakdm;
+	}
+
+	public int getFabeln() {
+		return fabeln;
+	}
+
+	public void setFabeln(int fabeln) {
+		this.fabeln = fabeln;
+	}
+
+	public String getFask() {
+		return fask;
+	}
+
+	public void setFask(String fask) {
+		this.fask = fask;
+	}
+
+	public String getFavk() {
+		return favk;
+	}
+
+	public void setFavk(String favk) {
+		this.favk = favk;
+	}
+
+	public String getFavt() {
+		return favt;
+	}
+
+	public void setFavt(String favt) {
+		this.favt = favt;
+	}
 
 	public String getFaval() {
 		return faval;
@@ -19,14 +333,6 @@ public class FaktDao implements IDao {
 
 	public void setFaval(String faval) {
 		this.faval = faval;
-	}
-
-	public int getFabelv() {
-		return fabelv;
-	}
-
-	public void setFabelv(int fabelv) {
-		this.fabelv = fabelv;
 	}
 
 	public int getFadato() {
--- src/main/no/systema/jservices/common/dao/HeadfDao.java
+++ src/main/no/systema/jservices/common/dao/HeadfDao.java
@@ -13,6 +13,330 @@ public class HeadfDao implements IDao {
 	private String helka;
 	private String heot;
 	private String hekdpl;
+	private String hesg;
+	private int hedtr;
+	private String hesdff;
+	private String hesdt;
+	private String hesdvt;
+	private String hegn;
+	private String hegnn;
+	private int hepro;
+	private String heplan;
+	private int trrund;
+	private String hesaml;
+	private String hehawb;
+	private String hesdla;
+	private String hesdl;
+	private String hevalt;
+	private int hevalp;
+	private int heknt;
+	private int hekns;
+	private String henas;
+	private String hekdfs;
+	private String hevals;
+	private int heknsf;
+	private int heans;
+	private String henasf;
+	private int heknk;
+	private String henak;
+	private String hekdfk;
+	private String hevalk;
+	private int heknkf;
+	private int heank;
+	private String henakf;
+	private String hekft1;
+	private String heft1;
+	private int hedtcp;
+	private int hedtmp;
+	private String hekdkk;
+
+	public int getHevalp() {
+		return hevalp;
+	}
+
+	public void setHevalp(int hevalp) {
+		this.hevalp = hevalp;
+	}
+
+	public int getHedtmp() {
+		return hedtmp;
+	}
+
+	public void setHedtmp(int hedtmp) {
+		this.hedtmp = hedtmp;
+	}
+
+	public String getHekft1() {
+		return hekft1;
+	}
+
+	public void setHekft1(String hekft1) {
+		this.hekft1 = hekft1;
+	}
+
+	public String getHeft1() {
+		return heft1;
+	}
+
+	public void setHeft1(String heft1) {
+		this.heft1 = heft1;
+	}
+
+	public int getHedtcp() {
+		return hedtcp;
+	}
+
+	public void setHedtcp(int hedtcp) {
+		this.hedtcp = hedtcp;
+	}
+
+	public String getHekdkk() {
+		return hekdkk;
+	}
+
+	public void setHekdkk(String hekdkk) {
+		this.hekdkk = hekdkk;
+	}
+
+	public String getHekdfk() {
+		return hekdfk;
+	}
+
+	public void setHekdfk(String hekdfk) {
+		this.hekdfk = hekdfk;
+	}
+
+	public String getHevalk() {
+		return hevalk;
+	}
+
+	public void setHevalk(String hevalk) {
+		this.hevalk = hevalk;
+	}
+
+	public int getHeknkf() {
+		return heknkf;
+	}
+
+	public void setHeknkf(int heknkf) {
+		this.heknkf = heknkf;
+	}
+
+	public int getHeank() {
+		return heank;
+	}
+
+	public void setHeank(int heank) {
+		this.heank = heank;
+	}
+
+	public String getHenakf() {
+		return henakf;
+	}
+
+	public void setHenakf(String henakf) {
+		this.henakf = henakf;
+	}
+
+	public int getHekns() {
+		return hekns;
+	}
+
+	public void setHekns(int hekns) {
+		this.hekns = hekns;
+	}
+
+	public String getHenas() {
+		return henas;
+	}
+
+	public void setHenas(String henas) {
+		this.henas = henas;
+	}
+
+	public String getHekdfs() {
+		return hekdfs;
+	}
+
+	public void setHekdfs(String hekdfs) {
+		this.hekdfs = hekdfs;
+	}
+
+	public String getHevals() {
+		return hevals;
+	}
+
+	public void setHevals(String hevals) {
+		this.hevals = hevals;
+	}
+
+	public int getHeknsf() {
+		return heknsf;
+	}
+
+	public void setHeknsf(int heknsf) {
+		this.heknsf = heknsf;
+	}
+
+	public int getHeans() {
+		return heans;
+	}
+
+	public void setHeans(int heans) {
+		this.heans = heans;
+	}
+
+	public String getHenasf() {
+		return henasf;
+	}
+
+	public void setHenasf(String henasf) {
+		this.henasf = henasf;
+	}
+
+	public int getHeknk() {
+		return heknk;
+	}
+
+	public void setHeknk(int heknk) {
+		this.heknk = heknk;
+	}
+
+	public String getHenak() {
+		return henak;
+	}
+
+	public void setHenak(String henak) {
+		this.henak = henak;
+	}
+
+	public String getHesaml() {
+		return hesaml;
+	}
+
+	public void setHesaml(String hesaml) {
+		this.hesaml = hesaml;
+	}
+
+	public String getHehawb() {
+		return hehawb;
+	}
+
+	public void setHehawb(String hehawb) {
+		this.hehawb = hehawb;
+	}
+
+	public String getHesdla() {
+		return hesdla;
+	}
+
+	public void setHesdla(String hesdla) {
+		this.hesdla = hesdla;
+	}
+
+	public String getHesdl() {
+		return hesdl;
+	}
+
+	public void setHesdl(String hesdl) {
+		this.hesdl = hesdl;
+	}
+
+	public String getHevalt() {
+		return hevalt;
+	}
+
+	public void setHevalt(String hevalt) {
+		this.hevalt = hevalt;
+	}
+
+	public int getHeknt() {
+		return heknt;
+	}
+
+	public void setHeknt(int heknt) {
+		this.heknt = heknt;
+	}
+
+	public String getHesdt() {
+		return hesdt;
+	}
+
+	public void setHesdt(String hesdt) {
+		this.hesdt = hesdt;
+	}
+
+	public String getHesdvt() {
+		return hesdvt;
+	}
+
+	public void setHesdvt(String hesdvt) {
+		this.hesdvt = hesdvt;
+	}
+
+	public String getHegn() {
+		return hegn;
+	}
+
+	public void setHegn(String hegn) {
+		this.hegn = hegn;
+	}
+
+	public String getHegnn() {
+		return hegnn;
+	}
+
+	public void setHegnn(String hegnn) {
+		this.hegnn = hegnn;
+	}
+
+	public int getHepro() {
+		return hepro;
+	}
+
+	public void setHepro(int hepro) {
+		this.hepro = hepro;
+	}
+
+	public String getHeplan() {
+		return heplan;
+	}
+
+	public void setHeplan(String heplan) {
+		this.heplan = heplan;
+	}
+
+	public int getTrrund() {
+		return trrund;
+	}
+
+	public void setTrrund(int trrund) {
+		this.trrund = trrund;
+	}
+
+	public String getHesg() {
+		return hesg;
+	}
+
+	public void setHesg(String hesg) {
+		this.hesg = hesg;
+	}
+
+	public int getHedtr() {
+		return hedtr;
+	}
+
+	public void setHedtr(int hedtr) {
+		this.hedtr = hedtr;
+	}
+
+	public String getHesdff() {
+		return hesdff;
+	}
+
+	public void setHesdff(String hesdff) {
+		this.hesdff = hesdff;
+	}
 
 	public int getHeavd() {
 		return heavd;
