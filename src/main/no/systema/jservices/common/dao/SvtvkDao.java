da48eeb8207c4fbdbc7861e38fdbddcd0c6b7b35
Oscar de la Torre <oscar@systema.no>
2017-05-19 12:54:17 +0200

    TDS - Vedlikehold firmanivå - Valutakurser

---
+++ src/main/no/systema/jservices/common/dao/SvtvkDao.java
@@ -0,0 +1,51 @@
+package no.systema.jservices.common.dao;
+import java.io.Serializable;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * All variables must be initialized to empty strings and NOT to NULL values
+ * This is because the db-inserts that will be done in all fields of the db-table
+ * 
+ * @author oscardelatorre
+ * @date Maj 19, 2017
+ * 
+ */
+public class SvtvkDao implements IDao  {
+
+	private String svvk_kd = "";                                
+	public String getSvvk_kdPropertyName (){ return "svvk_kd"; }
+	public void setSvvk_kd (String value){ this.svvk_kd = value;   }   
+	public String getSvvk_kd (){ return this.svvk_kd;   }  
+	
+	private String svvk_dts = "0"; 
+	public String getSvvk_dtsPropertyName (){ return "svvk_dts"; }
+	public void setSvvk_dts (String value){ this.svvk_dts = value;   }   
+	public String getSvvk_dts (){ return this.svvk_dts;   }              
+
+	private String svvk_dte = "0";
+	public String getSvvk_dtePropertyName (){ return "svvk_dte"; }
+	public void setSvvk_dte (String value){ this.svvk_dte = value;   }   
+	public String getSvvk_dte (){ return this.svvk_dte;   }              
+
+	private String svvk_omr = "0";
+	public String getSvvk_omrPropertyName (){ return "svvk_omr"; }
+	public void setSvvk_omr (String value){ this.svvk_omr = value;   }   
+	public String getSvvk_omr (){ return this.svvk_omr;   }              
+
+	private String svvk_krs = "0"; 
+	public String getSvvk_krsPropertyName (){ return "svvk_krs"; }
+	public void setSvvk_krs (String value){ this.svvk_krs = value;   }   
+	public String getSvvk_krs (){ return this.svvk_krs;   }              
+
+	Map<String, Object> keys = new HashMap<String, Object>();
+	public void setKeys(Map<String, Object> keys) { this.keys = keys; }
+	@Override
+	public Map<String, Object> getKeys() {
+		keys.put("svvk_kd", svvk_kd);
+		
+		return keys;
+	}
+	
+}
---
+++ src/main/no/systema/jservices/common/dao/services/SvtvkDaoServiceImpl.java
@@ -0,0 +1,7 @@
+package no.systema.jservices.common.dao.services;
+
+import no.systema.jservices.common.dao.SvtvkDao;
+
+public class SvtvkDaoServiceImpl extends GenericDaoServiceImpl<SvtvkDao> implements SvtvkDaoService{
+
+}
