9c8958f2dbe2b5996c4815a7b89eab7a43ecb3c0
Oscar de la Torre <oscar@systema.no>
2017-08-15 10:34:50 +0200

    Rollback on early check in

--- src/main/no/systema/jservices/common/dao/services/TrkodfDaoService.java
+++
@@ -1,9 +0,0 @@
-package no.systema.jservices.common.dao.services;
-
-import no.systema.jservices.common.dao.TrkodfDao;
-
-public interface TrkodfDaoService extends GenericDaoService<TrkodfDao> {
-
-
-
-}
