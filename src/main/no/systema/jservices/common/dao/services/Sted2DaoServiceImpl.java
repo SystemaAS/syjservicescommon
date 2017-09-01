79ab1385a3dc35e863425eaa715d2cbb1f2696bb
Fredrik Möller <fredrik@systema.no>
2017-09-01 11:18:37 +0200

    enhancement

--- src/main/no/systema/jservices/common/dao/services/Sted2DaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/Sted2DaoServiceImpl.java
@@ -1,8 +1,6 @@
 package no.systema.jservices.common.dao.services;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import no.systema.jservices.common.dao.Sted2Dao;
 
@@ -10,7 +8,6 @@ public class Sted2DaoServiceImpl extends GenericDaoServiceImpl<Sted2Dao> impleme
 
 	@Override
 	public List<Sted2Dao> findByLike(String st2kod) {
-		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("st2kod", WILD_CARD + st2kod + WILD_CARD);
 		return findAll(params);
 	}
