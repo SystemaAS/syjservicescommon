90aaa4e9aaadbcb0089c180348860ea6a6bb3c4c
Fredrik Möller <fredrik@systema.no>
2017-10-23 14:15:35 +0200

    Added check on f0077

--- src/main/no/systema/jservices/common/dao/services/FortollingDaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/FortollingDaoServiceImpl.java
@@ -3,7 +3,6 @@ package no.systema.jservices.common.dao.services;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -41,7 +40,6 @@ public class FortollingDaoServiceImpl extends GenericDaoServiceImpl<FortollingDt
 		if (!qDto.getAvdelingList().isEmpty()) {
 			queryString.append("    and  (shX.siavd IN ( :avdelingList ) )");
 		}		
-		//queryString.append(" 		and   (:avdeling = 0 OR shX.siavd = :avdeling )");
 		queryString.append(" 		and   (:deklarasjonsnr = 0 OR shX.sitdn = :deklarasjonsnr )");
 		queryString.append(" 		and   shX.siavd > 0 "); //sanity check
 		queryString.append(" 		and   shX.sitdn > 0 "); //sanity check
@@ -49,15 +47,14 @@ public class FortollingDaoServiceImpl extends GenericDaoServiceImpl<FortollingDt
 		if (!qDto.getSignaturList().isEmpty()) {
 			queryString.append("    and  (shX.sisg IN ( :signaturList )) ");
 		}
-		//queryString.append(" 		and   (:signatur IS NULL OR shX.sisg = :signatur)) as sh ");	
 		queryString.append(" ) as sh ");
 		queryString.append(" JOIN SADV sv  ");
 		queryString.append("	 ON sh.avdeling = sv.svavd AND  sh.deklarasjonsnr = sv.svtdn ");
-		queryString.append(" LEFT OUTER JOIN (select e.mavd mavd , e.mtdn mtdn, t.f4815 kalle, t.f0078a anka ");
+		queryString.append(" LEFT OUTER JOIN (select e.mavd mavd , e.mtdn mtdn, t.f4815 kalle, COALESCE(t.f0078a,t.f0077) anka ");
 		queryString.append(" 				  from EDIM e, TVINF t ");
 		queryString.append(" 				  where e.mmn = t.fmn ");
 		queryString.append(" 				  and   e.msr = 'R' and   e.m0065 = 'CUSRES' and   e.m1n07 = 'DME' ");
-		queryString.append(" 				  and   t.f0078a in('950','954','972','999') ");
+		queryString.append(" 				  and   ( t.f0078a in('950','954','972','999') OR  NULLIF(t.f0077, '') IS NOT NULL )");
 		queryString.append(" 				  and   t.f4815 in('NE','PP') ) e ");
 		queryString.append("  	ON sh.avdeling = e.mavd AND sh.deklarasjonsnr = e.mtdn ");
 		queryString.append(" group by  sh.avdeling, sh.deklarasjonsnr, sh.registreringsdato, sh.signatur,  sh.mottaker, COALESCE(concat(e.kalle, e.anka), 'OK') ");
@@ -68,7 +65,8 @@ public class FortollingDaoServiceImpl extends GenericDaoServiceImpl<FortollingDt
 		logger.info("getImportStats list.size="+list.size());
 		return list;
 	}	
-
+	
+	
 	private List<FortollingDto> getExportStats(FortollingDto qDto) {
 		NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(getJdbcTemplate().getDataSource());
 		SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(qDto);
@@ -82,7 +80,6 @@ public class FortollingDaoServiceImpl extends GenericDaoServiceImpl<FortollingDt
 		if (!qDto.getAvdelingList().isEmpty()) {
 			queryString.append("    and  (shX.seavd IN ( :avdelingList ) )");
 		}	
-		//queryString.append(" 		and   (:avdeling = 0 OR shX.seavd = :avdeling )");
 		queryString.append(" 		and   (:deklarasjonsnr = 0 OR shX.setdn = :deklarasjonsnr )");
 		queryString.append(" 		and   shX.seavd > 0 "); //sanity check
 		queryString.append(" 		and   shX.setdn > 0 "); //sanity check
@@ -91,14 +88,13 @@ public class FortollingDaoServiceImpl extends GenericDaoServiceImpl<FortollingDt
 			queryString.append("    and  (shX.sesg IN ( :signaturList )) ");
 		}		
 		queryString.append(" ) as sh ");
-		//queryString.append(" 		and   (:signatur IS NULL OR shX.sesg = :signatur)) as sh ");	
 		queryString.append(" JOIN SAEV sv  ");
 		queryString.append("	 ON sh.avdeling = sv.svavd AND  sh.deklarasjonsnr = sv.svtdn ");
-		queryString.append(" LEFT OUTER JOIN (select e.mavd mavd , e.mtdn mtdn, t.f4815 kalle, t.f0078a anka ");
+		queryString.append(" LEFT OUTER JOIN (select e.mavd mavd , e.mtdn mtdn, t.f4815 kalle, COALESCE(t.f0078a,t.f0077) anka ");
 		queryString.append(" 				  from EDIM e, TVINF t ");
 		queryString.append(" 				  where e.mmn = t.fmn ");
 		queryString.append(" 				  and   e.msr = 'R' and   e.m0065 = 'CUSRES' and   e.m1n07 = 'DME' ");
-		queryString.append(" 				  and   t.f0078a in('950','954','972','999') ");
+		queryString.append(" 				  and   ( t.f0078a in('950','954','972','999') OR NULLIF(t.f0077, '') IS NOT NULL )");
 		queryString.append(" 				  and   t.f4815 in('NE','PP') ) e ");
 		queryString.append("  	ON sh.avdeling = e.mavd AND sh.deklarasjonsnr = e.mtdn ");
 		queryString.append(" group by sh.avdeling, sh.deklarasjonsnr, sh.registreringsdato, sh.signatur,  sh.mottaker, COALESCE(concat(e.kalle, e.anka), 'OK') ");
@@ -110,5 +106,6 @@ public class FortollingDaoServiceImpl extends GenericDaoServiceImpl<FortollingDt
 		logger.info("getExportStats list.size="+list.size());
 		return list;
 	}	
+
 	
 }
