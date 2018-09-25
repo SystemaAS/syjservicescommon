f966c0c750775a34fe5596e91fca0fdff3874dcd
Fredrik Möller <fredrik@systema.no>
2018-09-25 13:23:53 +0200

    Added explicit findAll

--- src/main/no/systema/jservices/common/dao/services/KostaDaoService.java
+++ src/main/no/systema/jservices/common/dao/services/KostaDaoService.java
@@ -1,7 +1,24 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.List;
+
 import no.systema.jservices.common.dao.KostaDao;
 
 public interface KostaDaoService extends GenericDaoService<KostaDao> {
+	
+	/**
+	 * Find all {@linkplain KostaDao} on provided search-criteria. <br>
+	 * param can be null.
+	 * 
+	 * @param bilagsnr
+	 * @param innregnr
+	 * @param faktnr
+	 * @param levnr
+	 * @param attkode
+	 * @param komment
+	 * @param fradato (greater than)
+	 * @return a List of {@linkplain KostaDao}
+	 */
+	public List<KostaDao> findAll(String bilagsnr, String innregnr, String faktnr, String levnr, String attkode, String komment,  Number fradato);
 	
 }
--- src/main/no/systema/jservices/common/dao/services/KostaDaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/KostaDaoServiceImpl.java
@@ -1,7 +1,39 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import no.systema.jservices.common.dao.KostaDao;
 
 public class KostaDaoServiceImpl extends GenericDaoServiceImpl<KostaDao> implements KostaDaoService{
+
+	@Override
+	public List<KostaDao> findAll(String bilagsnr, String innregnr, String faktnr, String levnr, String attkode, String komment, Number fradato) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		if (innregnr != null) {
+			params.put("kabnr", innregnr);
+		}
+		if (bilagsnr != null) {
+			params.put("kabnr2", bilagsnr);
+		}
+		if (faktnr != null) {
+			params.put("kafnr", faktnr);
+		}
+		if (levnr != null) {
+			params.put("kalnr", levnr);
+		}
+		if (attkode != null) {
+			params.put("kasg", attkode);
+		}
+		if (komment != null) {
+			params.put("katxt", komment);
+		}
+		if (fradato != null) {
+			params.put("kabdt" + GREATER_AND_EQUALS_THEN, fradato);
+		}
+	
+		return findAll(params);
+	}
 
 }
