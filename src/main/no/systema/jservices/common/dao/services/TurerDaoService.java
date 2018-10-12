dd8f6ab7207db08bab570f2107abe30ad27721bd
Oscar de la Torre <oscar@systema.no>
2018-10-12 17:03:23 +0200

    Update - TURER light-weight DAO - Godsreg. uppslag

---
+++ src/main/no/systema/jservices/common/dao/services/TurerDaoService.java
@@ -0,0 +1,16 @@
+package no.systema.jservices.common.dao.services;
+
+
+import java.util.List;
+
+import no.systema.jservices.common.dao.TurerDao;
+
+
+/**
+ * 
+ * @author oscardelatorre
+ * @date Oct 2018
+ */
+public interface TurerDaoService extends GenericDaoService<TurerDao> {
+	
+}
---
+++ src/main/no/systema/jservices/common/dao/services/TurerDaoServiceImpl.java
@@ -0,0 +1,12 @@
+package no.systema.jservices.common.dao.services;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import no.systema.jservices.common.dao.TurerDao;
+
+public class TurerDaoServiceImpl extends GenericDaoServiceImpl<TurerDao> implements TurerDaoService{
+	
+
+}
