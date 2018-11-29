8e382c03cdf353e1cc7714c5c2b2227e65e33dca
Fredrik Möller <fredrik@systema.no>
2018-11-29 15:07:21 +0100

    Added transform: dao to dto

--- src/main/no/systema/jservices/common/dto/KostaDto.java
+++ src/main/no/systema/jservices/common/dto/KostaDto.java
@@ -19,6 +19,40 @@ public class KostaDto extends KostaDao {
 	private String fssok;
 	private String opp_dato;
 	private String reg_dato;
+	private String levnavn;
 
-	
+	/**
+	 * Convenience method for transforming Dao to Dto.
+	 * 
+	 * @param dao
+	 * @return KostaDto
+	 */
+	static public KostaDto get(KostaDao dao) {
+		KostaDto dto = new KostaDto();
+		
+		dto.setKabb(dao.getKabb());
+		dto.setKabdt(dao.getKabdt());
+		dto.setKabl(dao.getKabl());
+		dto.setKabnr(dao.getKabnr());
+		dto.setKabnr2(dao.getKabnr2());
+		dto.setKadte(dao.getKadte());
+		dto.setKadtr(dao.getKadtr());
+		dto.setKafdt(dao.getKafdt());
+		dto.setKaffdt(dao.getKaffdt());
+		dto.setKafnr(dao.getKafnr());
+		dto.setKalkid(dao.getKalkid());
+		dto.setKalnr(dao.getKalnr());
+		dto.setKapmn(dao.getKapmn());
+		dto.setKAPÅR(dao.getKAPÅR());
+		dto.setKasg(dao.getKasg());
+		dto.setKast(dao.getKast());
+		dto.setKatdr(dao.getKatdr());
+		dto.setKatme(dao.getKatme());
+		dto.setKatxt(dao.getKatxt());
+		dto.setKauser(dao.getKauser());
+		
+		return dto;
+		
+		
+	}
 }
