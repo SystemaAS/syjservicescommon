af0070cf08d4a86ddf2c6951488d3a468e218271
Oscar de la Torre <oscar@systema.no>
2019-01-31 12:22:51 +0100

    Update - Tradevision service (FFR00FDAOSERVICE)

---
+++ src/main/no/systema/jservices/common/dao/services/Ffr00fDaoService.java
@@ -0,0 +1,17 @@
+package no.systema.jservices.common.dao.services;
+
+
+import java.util.List;
+
+import no.systema.jservices.common.dao.Ffr00fDao;
+
+
+/**
+ * 
+ * @author oscardelatorre
+ * @date Dec 2018
+ */
+public interface Ffr00fDaoService extends GenericDaoService<Ffr00fDao> {
+	
+	
+}
---
+++ src/main/no/systema/jservices/common/dao/services/Ffr00fDaoServiceImpl.java
@@ -0,0 +1,8 @@
+package no.systema.jservices.common.dao.services;
+
+import no.systema.jservices.common.dao.Ffr00fDao;
+
+public class Ffr00fDaoServiceImpl extends GenericDaoServiceImpl<Ffr00fDao> implements Ffr00fDaoService{
+	
+	
+}
