fee0ab6e27ea301a205158a61068e5b2d196bbd8
Fredrik Möller <fredrik@systema.no>
2019-02-18 11:12:02 +0100

    Added findByLike

--- src/main/no/systema/jservices/common/dao/services/KodtgeDaoService.java
+++ src/main/no/systema/jservices/common/dao/services/KodtgeDaoService.java
@@ -1,7 +1,10 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.List;
+
 import no.systema.jservices.common.dao.KodtgeDao;
 
 public interface KodtgeDaoService extends GenericDaoService<KodtgeDao> {
 	
+	List<KodtgeDao> findByLike(String kgekod);
 }
--- src/main/no/systema/jservices/common/dao/services/KodtgeDaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/KodtgeDaoServiceImpl.java
@@ -1,7 +1,18 @@
 package no.systema.jservices.common.dao.services;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import no.systema.jservices.common.dao.KodtgeDao;
 
 public class KodtgeDaoServiceImpl extends GenericDaoServiceImpl<KodtgeDao> implements KodtgeDaoService{
+
+	@Override
+	public List<KodtgeDao> findByLike(String kgekod) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("kgekod", WILD_CARD + kgekod + WILD_CARD);
+		return findAll(params);
+	}
 	
 }
