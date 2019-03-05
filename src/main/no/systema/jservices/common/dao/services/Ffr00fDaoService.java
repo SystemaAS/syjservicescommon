d5349cd4438fab0a35f884b0097e51151750b14b
Oscar de la Torre <oscar@systema.no>
2019-03-05 11:37:02 +0100

    Update - Fix

--- src/main/no/systema/jservices/common/dao/services/Ffr00fDaoService.java
+++ src/main/no/systema/jservices/common/dao/services/Ffr00fDaoService.java
@@ -1,13 +1,7 @@
 package no.systema.jservices.common.dao.services;
 
 
-import java.util.List;
-import java.util.Map;
-
-import org.modelmapper.ModelMapper;
-
 import no.systema.jservices.common.dao.Ffr00fDao;
-import no.systema.jservices.common.dao.modelmapper.converter.DaoConverter;
 import no.systema.jservices.common.dto.Ffr00fDto;
 import no.systema.jservices.common.dao.facade.Ffr00fDaoFacade;
 
@@ -17,9 +11,9 @@ import no.systema.jservices.common.dao.facade.Ffr00fDaoFacade;
  * @date Jan 2019
  */
 public interface Ffr00fDaoService extends GenericDaoService<Ffr00fDao> {
-	/*public List<Ffr00fDto> findAll(Map<String, Object> params, Ffr00fDaoFacade f);
+	
 	public Ffr00fDao create(Ffr00fDto t, Ffr00fDaoFacade f);
 	public Ffr00fDao update(Ffr00fDto t, Ffr00fDaoFacade f);
 	public void delete(Ffr00fDto t, Ffr00fDaoFacade f);
-	*/
+	
 }
--- src/main/no/systema/jservices/common/dao/services/Ffr00fDaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/Ffr00fDaoServiceImpl.java
@@ -14,58 +14,18 @@ import no.systema.jservices.common.dao.Ffr04fDao;
 
 public class Ffr00fDaoServiceImpl extends GenericDaoServiceImpl<Ffr00fDao> implements Ffr00fDaoService{
 	
-	/**
-	 * The method returns a complete dto (cascade of all dao's in a parent-child retaltionship)
-	 * In this way the result will be used in a single view element based on this dto
-	 *  
-	 */
-	/*
-	public List<Ffr00fDto> findAll(Map<String, Object> params, Ffr00fDaoFacade f){
-		Ffr00fDto dto = f.getDto();
-		
-		List<Ffr00fDto> listDto = new ArrayList<Ffr00fDto>();
-		List<Ffr00fDao> listDao = super.findAll(params);
-		
-		for (Ffr00fDao record: listDao){
-			//parent record
-			f.setDto(record);
-			//03
-			List<Ffr03fDao> list03 = new ArrayList<Ffr03fDao>();
-			list03 = ffr03fDaoService.findAll(record.getKeys());
-			dto.setListFfr03fDao(list03);
-			
-			03
-			Ffr03fDao dao03 = new Ffr03fDao();
-			dao03.setF03rec(record.getF00rec());
-			dao03 = ffr03fDaoService.find(dao03);
-			if(dao03!=null){
-				f.setDto(dao03);
-			}
-			//04
-			Ffr04fDao dao04 = new Ffr04fDao();
-			dao04.setF04rec(record.getF00rec());
-			dao04 = ffr04fDaoService.find(dao04);
-			if(dao04!=null){
-				f.setDto(dao04);
-			}
 	
-			//set final list with complete dto
-			listDto.add(f.getDto());
-		}
-		
-		return listDto;
-	}
 	
 	 
 	public Ffr00fDao create(Ffr00fDto dto, Ffr00fDaoFacade facade){
 		//Create cascade
 		Ffr00fDao rDaoParent = super.create(facade.getFfr00fDao());
 		ffr03fDaoService.create(facade.getFfr03fDao());
-		ffr04fDaoService.create(facade.getFfr04fDao());
+		/*ffr04fDaoService.create(facade.getFfr04fDao());
 		ffr08fDaoService.create(facade.getFfr08fDao());
 		ffr10fDaoService.create(facade.getFfr10fDao());
 		ffr11fDaoService.create(facade.getFfr11fDao());
-		
+		*/
 		return rDaoParent;
 	}
 	
@@ -77,7 +37,7 @@ public class Ffr00fDaoServiceImpl extends GenericDaoServiceImpl<Ffr00fDao> imple
 		ffr08fDaoService.update(facade.getFfr08fDao());
 		ffr10fDaoService.update(facade.getFfr10fDao());
 		ffr11fDaoService.update(facade.getFfr11fDao());
-		
+		*/
 		
 		return rDaoParent;
 	}
@@ -85,11 +45,11 @@ public class Ffr00fDaoServiceImpl extends GenericDaoServiceImpl<Ffr00fDao> imple
 	public void delete(Ffr00fDto dto, Ffr00fDaoFacade facade ){
 		super.delete(facade.getFfr00fDao());
 		ffr03fDaoService.delete(facade.getFfr03fDao());
-		ffr04fDaoService.delete(facade.getFfr04fDao());
+		/*ffr04fDaoService.delete(facade.getFfr04fDao());
 		ffr08fDaoService.delete(facade.getFfr08fDao());
 		ffr10fDaoService.delete(facade.getFfr10fDao());
 		ffr11fDaoService.delete(facade.getFfr11fDao());
-		
+		*/
 	}
 	
 	//Wired
@@ -112,6 +72,6 @@ public class Ffr00fDaoServiceImpl extends GenericDaoServiceImpl<Ffr00fDao> imple
 	private Ffr11fDaoService ffr11fDaoService = null;
 	public void setFfr11fDaoService (Ffr11fDaoService ffr11fDaoService) {this.ffr11fDaoService = ffr11fDaoService;}
 	public Ffr11fDaoService getFfr11fDaoService() { return this.ffr11fDaoService; }
-	*/
+	
 	
 }
