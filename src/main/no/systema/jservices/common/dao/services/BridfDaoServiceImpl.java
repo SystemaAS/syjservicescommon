ce140d6e02b0a73ee6af3a6fa5c7faeb13cbf251
Oscar de la Torre <oscar@systema.no>
2020-01-09 15:50:22 +0100

    XSS on BRIDF-service (syjcommon) for visma-proxy

--- src/main/no/systema/jservices/common/dao/services/BridfDaoServiceImpl.java
+++ src/main/no/systema/jservices/common/dao/services/BridfDaoServiceImpl.java
@@ -9,6 +9,7 @@ public class BridfDaoServiceImpl extends GenericDaoServiceImpl<BridfDao> impleme
 	@Override
 	public String getUserName(String name) {
 		BridfDao qDao = new BridfDao();
+		name = this.cleanXSS(name);
 		qDao.setBibrid(name);
 		BridfDao resultDao = find(qDao);
 	
@@ -22,6 +23,7 @@ public class BridfDaoServiceImpl extends GenericDaoServiceImpl<BridfDao> impleme
 
 	@Override
 	public boolean userNameExist(String name) {
+		name = this.cleanXSS(name);
 		params.put("bibrid", name);
 		List<BridfDao> resultList = findAll(params);
 	
@@ -30,6 +32,32 @@ public class BridfDaoServiceImpl extends GenericDaoServiceImpl<BridfDao> impleme
 		} else {
 			return true;
 		}
+	}
+	
+	/**
+	 * clean user from XSS-attacks 
+	 * @param value
+	 * @return
+	 */
+	private String cleanXSS(String value) {
+		// You'll need to remove the spaces from the html entities below
+		logger.info("InnXSS RequestWrapper ..............." + value);
+		//value = value.replaceAll("<", "& lt;").replaceAll(">", "& gt;");
+		//value = value.replaceAll("\\(", "& #40;").replaceAll("\\)", "& #41;");
+		//value = value.replaceAll("'", "& #39;");
+		value = value.replaceAll("eval\\((.*)\\)", "");
+		value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+
+		value = value.replaceAll("(?i)<script.*?>.*?<script.*?>", "");
+		value = value.replaceAll("(?i)<script.*?>.*?</script.*?>", "");
+		value = value.replaceAll("(?i)<.*?javascript:.*?>.*?</.*?>", "");
+		value = value.replaceAll("(?i)<.*?\\s+on.*?>.*?</.*?>", "");
+		value = value.replaceAll("script", "");
+		//value = value.replaceAll("</script>", "");
+		
+		value = value.replaceAll("'", "");
+		logger.info("OutnXSS RequestWrapper ........ value ......." + value);
+		return value;
 	}
 
 }
