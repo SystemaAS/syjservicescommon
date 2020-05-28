80251f37c293f033e43d86723d36dde18999c930
Oscar de la Torre <oscar@systema.no>
2020-05-28 15:59:10 +0200

    Update

---
+++ src/main/no/systema/jservices/common/dto/CountryDto.java
@@ -0,0 +1,9 @@
+package no.systema.jservices.common.dto;
+
+import lombok.Data;
+
+@Data
+public class CountryDto {
+	private String countryCode;
+	private String translations;
+}
---
+++ src/main/no/systema/jservices/common/dto/UserDto.java
@@ -0,0 +1,10 @@
+package no.systema.jservices.common.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+	private String userId;
+	private String username;
+	private String name;
+}
