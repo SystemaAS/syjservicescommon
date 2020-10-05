bfc825ac4168b8bb9e0549717f687cefb1bb5b27
Oscar de la Torre <oscar@systema.no>
2020-10-05 11:29:48 +0200

    .

---
+++ src/main/no/systema/jservices/common/dto/expressfortolling/ManifestStatusDto.java
@@ -0,0 +1,12 @@
+package no.systema.jservices.common.dto.expressfortolling;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ManifestStatusDto {
+	private String manifestId;
+	private String status;
+	
+}
