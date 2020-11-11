404ce93d25842cc0edcf806685f40e7383f5c8ee
Oscar de la Torre <oscar@systema.no>
2020-11-11 22:28:53 +0100

    UCase - Tlagring - Arkivp

---
+++ src/main/no/systema/jservices/common/dao/ArkivpDao.java
@@ -0,0 +1,30 @@
+package no.systema.jservices.common.dao;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Data;
+
+@Data
+public class ArkivpDao implements IDao  {
+	String arstat = "A"; //TEGN 1 
+	String artype = "TL"; //TEGN 2 
+ 	String arfirm = ""; //TEGN 2 
+ 	String aravd = "0";  //SONET 4.0 
+	String aropd = "0";  //SONET 7.0  
+	String arunde = ""; //TEGN 10 
+	String aruser = ""; //TEGN 10
+	String ardate = "0"; //SONET 8.0
+	String artime = "0"; //SONET 6.0
+	String arkund = "0"; //SONET 8.0 
+	String arlink = ""; //TEGN 40 Unik link (pdfnamn)
+	String arrfk = "";  //TEGN 35 Kunde ref
+	String arfri = "";  //TEGN 15 Fri
+	String arbhis = ""; //TEGN 30 Bane historikk
+	
+	Map<String, Object> keys = new HashMap<String, Object>();
+
+	                                                                                    
+	                                                                                      
+}
